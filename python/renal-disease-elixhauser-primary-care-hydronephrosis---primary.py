2f49b72048967621c9d5a333f749324cdf46f71e
Martin Chapman <contact@martinchapman.co.uk>
2024-05-30 00:51:19 +0100

    Update made by Phenoflow

---
+++ python/renal-disease-elixhauser-primary-care-hydronephrosis---primary.py
@@ -0,0 +1,25 @@
+# David Metcalfe, James Masters, Antonella Delmestri, Andrew Judge, Daniel Perry, Cheryl Zogg, Belinda Gabbe, Matthew Costa, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"PD23.00","system":"readv2"},{"code":"K0y..00","system":"readv2"},{"code":"K03y000","system":"readv2"},{"code":"K03y.00","system":"readv2"},{"code":"K0...00","system":"readv2"},{"code":"K0z..00","system":"readv2"},{"code":"K013.11","system":"readv2"},{"code":"K01w000","system":"readv2"},{"code":"Kyu1100","system":"readv2"},{"code":"K03yz00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('renal-disease-elixhauser-primary-care-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["renal-disease-elixhauser-primary-care-hydronephrosis---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["renal-disease-elixhauser-primary-care-hydronephrosis---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["renal-disease-elixhauser-primary-care-hydronephrosis---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
